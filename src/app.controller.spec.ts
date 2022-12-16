196dd10f067d155d8bafdf015f45aa6bb346444f
atul_mane <atul.mane@tibilsolution.com>
2022-12-16 12:33:21 +0530

    added common service for quary exceution

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -19,4 +19,10 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  describe('sum module', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(appController.sum(1, 2)).toBe(3);
+    });
+  });
 });
