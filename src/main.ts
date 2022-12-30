ce9602e966773615c89b04a3ef42a69f11f58e2e
Theja G D <theja@appzui.com>
2022-12-30 12:21:38 +0530

    changed port number

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
+//  DONT CHANGE THE PORT NUMBER IT SHOULD BE 3001 ALWAYS
 }
 bootstrap();
