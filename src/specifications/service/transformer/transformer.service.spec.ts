a5d29124e3c8218eb5829985020922434418693c
atulmane01 <atul.mane@tibilsolution.com>
2023-01-16 15:15:01 +0530

    modified test cases with code coverage

--- src/specifications/service/transformer/transformer.service.spec.ts
+++ src/specifications/service/transformer/transformer.service.spec.ts
@@ -22,7 +22,6 @@ describe('TransformerService', () => {
         data: { code: 200, TransformerFiles: [], Message: "Transformer created succesfully", }
     }
 
-
     const mockHttpservice = {
         post: jest.fn().mockReturnValueOnce(apidata)
     };
@@ -62,7 +61,8 @@ describe('TransformerService', () => {
 
             // "ingestion_name":"students_attendance",
             "key_file": "transformer_dataset_maping.csv",
-            "program": "SAC"
+            "program": "SAC",
+            "operation": "dimension"
         };
         let result = {
             "code": 400, error: [
@@ -77,7 +77,7 @@ describe('TransformerService', () => {
                 },
             ]
         };
-        
+
         expect(await service.createTransformer(transformerData)).toStrictEqual(result);
     });
 
@@ -86,7 +86,7 @@ describe('TransformerService', () => {
             "ingestion_name": "students_attendance11",
             "key_file": "transformer_dataset_maping.csv",
             "program": "SAC",
-            "operation":"dataset"
+            "operation": "dataset"
         };
         let result = {
             "code": 400, error: "Invalid dataset name"
@@ -94,127 +94,129 @@ describe('TransformerService', () => {
         expect(await service.createTransformer(transformerData)).toStrictEqual(result);
     });
 
-    // it('unable to create a transformer', async () => {
-    //     let result = {
-    //         "code": 400, "error": "unable to create a transformer"
-    //     };
-    //     const transformerData = {
-    //         "ingestion_name": "students_attendance11",
-    //         "key_file": "transformer_dataset_maping.csv",
-    //         "program": "SAC"
-    //     };
-    //     expect(await service.createTransformer(transformerData)).toStrictEqual(result)
-    // });
+    it('unable to create a transformer', async () => {
+        let result = {
+            "code": 400, "error": "unable to create a transformer"
+        };
+        const transformerData = {
+            "ingestion_name": "students_attendance11",
+            "key_file": "transformer_dataset_maping.csv",
+            "program": "SAC",
+            "operation": "dataset"
+        };
+        expect(await service.createTransformer(transformerData)).toStrictEqual(result)
+    });
 
-    // it('transformer created sucessfully', async () => {
-    //     let service: TransformerService;
-    //     const mockTransacation = {
-    //         createQueryRunner: jest.fn().mockImplementation(() => ({
-    //             connect: jest.fn(),
-    //             startTransaction: jest.fn(),
-    //             release: jest.fn(),
-    //             rollbackTransaction: jest.fn(),
-    //             commitTransaction: jest.fn(),
-    //             query: jest.fn().mockReturnValueOnce([{ pid: 1 }])
-    //         })),
-    //         query: jest.fn().mockReturnValueOnce([{ length: 1 }]),
-    //     }
+    it('transformer not created', async () => {
+        let service: TransformerService;
+        const mockTransacation = {
+            createQueryRunner: jest.fn().mockImplementation(() => ({
+                connect: jest.fn(),
+                startTransaction: jest.fn(),
+                release: jest.fn(),
+                rollbackTransaction: jest.fn(),
+                commitTransaction: jest.fn(),
+                query: jest.fn().mockReturnValueOnce([{ pid: 1 }])
+            })),
+            query: jest.fn().mockReturnValueOnce([{ length: 1 }]),
+        }
 
-    //     let apidata = {
-    //         data: { code: 200, TransformerFiles: [{filename:"test"}], Message: "Transformer created succesfully", }
-    //     }
-    //     const mockHttpservice = {
-    //         post: jest.fn().mockReturnValueOnce(apidata)
-    //     };
+        let apidata = {
+            data: { TransformerFiles: [{ filename: "test" }], Message: "Transformer not created", }
+        }
+        const mockHttpservice = {
+            post: jest.fn().mockReturnValueOnce(apidata)
+        };
 
-    //     const module: TestingModule = await Test.createTestingModule({
-    //         providers: [TransformerService, DataSource, GenericFunction, HttpCustomService,
-                
-    //             {
-    //                 provide: DataSource,
-    //                 useValue: mockTransacation
-    //             },
-    //             {
-    //                 provide: GenericFunction,
-    //                 useClass: GenericFunction
-    //             },
-    //             {
-    //                 provide: HttpCustomService,
-    //                 useValue: mockHttpservice
-    //             },
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [TransformerService, DataSource, GenericFunction, HttpCustomService,
 
-    //         ]
-    //     }).compile();
+                {
+                    provide: DataSource,
+                    useValue: mockTransacation
+                },
+                {
+                    provide: GenericFunction,
+                    useClass: GenericFunction
+                },
+                {
+                    provide: HttpCustomService,
+                    useValue: mockHttpservice
+                },
 
-    //     service = module.get<TransformerService>(TransformerService);
-    //     const transformerData = {
-    //         "ingestion_name": "students_attendance11",
-    //         "key_file": "transformer_dataset_maping.csv",
-    //         "program": "SAC"
-    //     };
-    //     let result = {
-    //         "code": 200,
-    //         "message": "Transformer created succesfully",
-    //         "response": [{
-    //             "pid": 1,
-    //             "filename": "test"
-    //         }]
-    //     };
-    //     expect(await service.createTransformer(transformerData)).toStrictEqual(result)
-    // });
+            ]
+        }).compile();
 
-    // it('transformer not created', async () => {
-    //     let service: TransformerService;
-    //     const mockTransacation = {
-    //         createQueryRunner: jest.fn().mockImplementation(() => ({
-    //             connect: jest.fn(),
-    //             startTransaction: jest.fn(),
-    //             release: jest.fn(),
-    //             rollbackTransaction: jest.fn(),
-    //             commitTransaction: jest.fn(),
-    //             query: jest.fn().mockReturnValueOnce([{ pid: 1 }])
-    //         })),
-    //         query: jest.fn().mockReturnValueOnce([{ length: 1 }]),
-    //     }
+        service = module.get<TransformerService>(TransformerService);
+        const transformerData = {
+            "ingestion_name": "students_attendance11",
+            "key_file": "transformer_dataset_maping.csv",
+            "program": "SAC",
+            "operation": "dataset"
+        };
+        let result = {
+            "code": 400, "error": "Transformer not created",
+        };
+        expect(await service.createTransformer(transformerData)).toStrictEqual(result)
+    });
 
-    //     let apidata = {
-    //         data: { TransformerFiles: [{filename:"test"}], Message: "Transformer not created", }
-    //     }
-    //     const mockHttpservice = {
-    //         post: jest.fn().mockReturnValueOnce(apidata)
-    //     };
+    it('transformer created sucessfully', async () => {
+        let service: TransformerService;
+        const mockTransacation = {
+            createQueryRunner: jest.fn().mockImplementation(() => ({
+                connect: jest.fn(),
+                startTransaction: jest.fn(),
+                release: jest.fn(),
+                rollbackTransaction: jest.fn(),
+                commitTransaction: jest.fn(),
+                query: jest.fn().mockReturnValueOnce([{ pid: 1 }])
+            })),
+            query: jest.fn().mockReturnValueOnce([{ length: 1 }]),
+        }
 
-    //     const module: TestingModule = await Test.createTestingModule({
-    //         providers: [TransformerService, DataSource, GenericFunction, HttpCustomService,
-                
-    //             {
-    //                 provide: DataSource,
-    //                 useValue: mockTransacation
-    //             },
-    //             {
-    //                 provide: GenericFunction,
-    //                 useClass: GenericFunction
-    //             },
-    //             {
-    //                 provide: HttpCustomService,
-    //                 useValue: mockHttpservice
-    //             },
+        let apidata = {
+            data: { code: 200, TransformerFiles: [{ filename: "test" }], Message: "Transformer created succesfully", }
+        }
+        const mockHttpservice = {
+            post: jest.fn().mockReturnValueOnce(apidata)
+        };
 
-    //         ]
-    //     }).compile();
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [TransformerService, DataSource, GenericFunction, HttpCustomService,
 
-    //     service = module.get<TransformerService>(TransformerService);
-    //     const transformerData = {
-    //         "ingestion_name": "students_attendance11",
-    //         "key_file": "transformer_dataset_maping.csv",
-    //         "program": "SAC",
-    //         "operation": "dataset"
-    //     };
-    //     let result = {
-    //         "code": 400,"error": "Invalid dataset name",
-    //     };
-    //     expect(await service.createTransformer(transformerData)).toStrictEqual(result)
-    // });
+                {
+                    provide: DataSource,
+                    useValue: mockTransacation
+                },
+                {
+                    provide: GenericFunction,
+                    useClass: GenericFunction
+                },
+                {
+                    provide: HttpCustomService,
+                    useValue: mockHttpservice
+                },
+
+            ]
+        }).compile();
+
+        service = module.get<TransformerService>(TransformerService);
+        const transformerData = {
+            "ingestion_name": "students_attendance11",
+            "key_file": "transformer_dataset_maping.csv",
+            "program": "SAC",
+            "operation": "dimension"
+        };
+        let result = {
+            "code": 200,
+            "message": "Transformer created succesfully",
+            "response": [{
+                "pid": 1,
+                "filename": "test"
+            }]
+        };
+        expect(await service.createTransformer(transformerData)).toStrictEqual(result)
+    });
 
     it('exception', async () => {
         let service: TransformerService;
@@ -225,9 +227,7 @@ describe('TransformerService', () => {
                 release: jest.fn(),
                 rollbackTransaction: jest.fn(),
                 commitTransaction: jest.fn(),
-                query: jest.fn().mockReturnValueOnce([{ pid: 1 }]).mockImplementation(() => {
-                    throw Error("exception test")
-                })
+                query: jest.fn().mockReturnValueOnce([{ pid: 1 }])
             })),
             query: jest.fn().mockReturnValueOnce([{ length: 1 }]),
         }
@@ -236,7 +236,9 @@ describe('TransformerService', () => {
             data: { code:200,TransformerFiles: [{filename:"test"}], Message: "Transformer not created", }
         }
         const mockHttpservice = {
-            post: jest.fn().mockReturnValueOnce(apidata)
+            post: jest.fn().mockImplementation(() => {
+                throw Error("exception test")
+            })
         };
 
         const module: TestingModule = await Test.createTestingModule({
@@ -262,7 +264,8 @@ describe('TransformerService', () => {
         const transformerData = {
             "ingestion_name": "students_attendance11",
             "key_file": "transformer_dataset_maping.csv",
-            "program": "SAC"
+            "program": "SAC",
+            "operation": "dimension"
         };
         let resultOutput = "Error: exception test";
 
@@ -273,4 +276,5 @@ describe('TransformerService', () => {
         }
        
     });
+
 });
